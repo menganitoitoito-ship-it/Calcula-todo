c83cdcdac1d6c415a77221aa2c9022e046e19166
menganitoitoito-ship-it <menganitoitoito@gmail.com>
2026-07-09 14:16:14 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,21 @@
-const CACHE = 'kalk-v1';
-const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).then(res => { const cl=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,cl)); return res; }).catch(()=>caches.match(e.request))); });
+var CACHE = 'kalk-v' + new Date().toISOString().slice(0,10).replace(/-/g,'');
+var ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(ASSETS); }));
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).then(function(res) {
+      var cl = res.clone();
+      caches.open(CACHE).then(function(c) { c.put(e.request, cl); });
+      return res;
+    }).catch(function() { return caches.match(e.request); })
+  );
+});
